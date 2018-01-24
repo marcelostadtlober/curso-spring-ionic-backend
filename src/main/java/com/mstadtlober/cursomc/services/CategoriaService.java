70747d68676c6c93454a3d3111eb68986cf2c856
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2018-01-24 21:55:10 -0200

    Criando repository e service para Categoria

---
+++ src/main/java/com/mstadtlober/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.mstadtlober.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mstadtlober.cursomc.domain.Categoria;
+import com.mstadtlober.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		
+		return obj;
+	}
+
+}
