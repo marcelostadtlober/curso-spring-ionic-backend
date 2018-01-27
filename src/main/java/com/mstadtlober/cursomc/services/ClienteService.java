2a829ca17509e66d9798f76c582c7bd00700b6e1
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2018-01-26 22:47:12 -0200

    Endpoint /cliente /{id} disponivel

---
+++ src/main/java/com/mstadtlober/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.mstadtlober.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mstadtlober.cursomc.domain.Cliente;
+import com.mstadtlober.cursomc.repositories.ClienteRepository;
+import com.mstadtlober.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		
+		return obj;
+	}
+
+}
