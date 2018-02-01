f9e8062a081a1866d0973cdfbd0e891d1a8379b2
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2018-02-01 15:32:35 -0200

    Inserindo um novo cliente com POST

--- src/main/java/com/mstadtlober/cursomc/services/ClienteService.java
+++ src/main/java/com/mstadtlober/cursomc/services/ClienteService.java
@@ -9,9 +9,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.mstadtlober.cursomc.domain.Cidade;
 import com.mstadtlober.cursomc.domain.Cliente;
+import com.mstadtlober.cursomc.domain.Endereco;
+import com.mstadtlober.cursomc.domain.enums.TipoCliente;
 import com.mstadtlober.cursomc.dto.ClienteDTO;
+import com.mstadtlober.cursomc.dto.ClienteNewDTO;
+import com.mstadtlober.cursomc.repositories.CidadeRepository;
 import com.mstadtlober.cursomc.repositories.ClienteRepository;
+import com.mstadtlober.cursomc.repositories.EnderecoRepository;
 import com.mstadtlober.cursomc.services.exceptions.DataIntegrityException;
 import com.mstadtlober.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -20,6 +26,10 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
 		Cliente obj = repo.findOne(id);
@@ -27,6 +37,45 @@ public class ClienteService {
 			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
 					+ ", Tipo: " + Cliente.class.getName());
 		}
+		
+		return obj;
+	}
+	
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDTO) {
+//		throw new UnsupportedOperationException();
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = cidadeRepository.findOne(objDTO.getCidadeId());
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if (objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if (objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		
+		return cli;
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.save(obj.getEnderecos());
 		
 		return obj;
 	}
@@ -51,21 +100,6 @@ public class ClienteService {
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir um Cliente que possui pedido(s)!");
 		}
-	}
-
-	public List<Cliente> findAll() {
-		return repo.findAll();
-	}
-	
-	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
-		return repo.findAll(pageRequest);
-	}
-	
-	public Cliente fromDTO(ClienteDTO objDTO) {
-//		throw new UnsupportedOperationException();
-		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
 	}
 
 }
