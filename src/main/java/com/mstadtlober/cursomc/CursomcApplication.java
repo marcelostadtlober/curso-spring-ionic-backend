34816699bad40e69a4bfca7366db1a6d6e009803
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2018-02-19 21:55:46 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/mstadtlober/cursomc/CursomcApplication.java
+++ src/main/java/com/mstadtlober/cursomc/CursomcApplication.java
@@ -1,18 +1,25 @@
 package com.mstadtlober.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mstadtlober.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
-	public void run(String... arg0) throws Exception {		
+	public void run(String... arg0) throws Exception {
+		s3service.uploadFile("D:\\Marcelo\\Pictures\\Icones\\afterburner.ico");
 	}
 	
 }
