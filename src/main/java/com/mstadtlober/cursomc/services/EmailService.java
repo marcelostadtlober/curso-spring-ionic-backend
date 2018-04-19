5cb34d74b4c8ad746284dd591e1eaf188ce56562
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2018-04-19 20:03:07 -0300

    Email HTML

--- src/main/java/com/mstadtlober/cursomc/services/EmailService.java
+++ src/main/java/com/mstadtlober/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.mstadtlober.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.mstadtlober.cursomc.domain.Cliente;
@@ -12,5 +14,9 @@ public interface EmailService {
 	void sendEmail(SimpleMailMessage msg);
 
 	void sendNewPasswordEmail(Cliente cliente, String newPass);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
